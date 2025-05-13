46f361a74f1405b4974efce944de9a78a3608f4e
Cyril Plisko <cyril.plisko@mountall.com>
2025-05-13 16:26:09 +0300

    errors: turn comments into docs

--- src/api/error.rs
+++ src/api/error.rs
@@ -16,13 +16,22 @@ pub struct Error {
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 pub enum Code {
-    UnknownApiKey,       //	Provided API Key is invalid.
-    ExpiredApiKey,       //	The provided API key has expired.
-    UnknownMethod,       //	Provided method is not supported.
-    WrongParams,         //	Some parameters is wrong.
-    NotFound,            //	Requested data was not found.
-    MinuteLimitExceeded, //	The number of requests per minute has been exceeded.
-    HourLimitExceeded,   //	The number of requests per hour has been exceeded.
-    MonthLimitExceeded,  //	The number of requests per month has been exceeded.
-    InternalError,       //	An internal error occurred.
+    /// Provided API Key is invalid.
+    UnknownApiKey,
+    /// The provided API key has expired.
+    ExpiredApiKey,
+    /// Provided method is not supported.
+    UnknownMethod,
+    /// Some parameters is wrong.
+    WrongParams,
+    /// Requested data was not found.
+    NotFound,
+    /// The number of requests per minute has been exceeded.
+    MinuteLimitExceeded,
+    /// The number of requests per hour has been exceeded.
+    HourLimitExceeded,
+    /// The number of requests per month has been exceeded.
+    MonthLimitExceeded,
+    /// An internal error occurred.
+    InternalError,
 }
