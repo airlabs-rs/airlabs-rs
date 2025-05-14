0d5d3055473f2cffb04e5165bd4ff3dc1e69322e
Cyril Plisko <cyril.plisko@mountall.com>
2025-05-14 12:30:11 +0300

    Add Airport::name and Airport::name_by_language

--- examples/airlines.rs
+++ examples/airlines.rs
@@ -1,7 +1,10 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let client = airlabs::Client::new("KAAA");
-    let airlines = client.airlines().await?;
+    let airlines = client
+        .airlines()
+        .await
+        .inspect_err(|err| println!("{err:#?}"))?;
     println!("{airlines:#?}");
     Ok(())
 }
